3eaddf9eacff06abd57ccbf7ecb939921611833d
Tushar Gupta <17tushar05gupta2000@gmail.com>
2023-03-17 04:14:12 +0530

    fixed typo again+ adding gpio but finished with errors will resolve in morning

--- gpio/gpio.cpp
+++ gpio/gpio.cpp
@@ -1,12 +1,12 @@
 #include "gpio.h"
 
-gpio::GPIO(int pin_number)
+ECC_gpio::GPIO::GPIO(int pin_number)
 {
 	std::cout << "exporting the pin" + string(pin_number) << std::endl;
 	system(string("echo" + string(pin_number) + " > " + string(path) + "export").cstr());
 }
 
-gpio::gpioInit(int pin_number, string pin_direction, string pin_mode = "none")
+ECC_gpio::GPIO::gpioInit(int pin_number, string pin_direction, string pin_mode = "none")
 {
 	fstream file;
     file.open((string(path) + "/gpio" + string(pin_number) + "/direction").c_str(), fstream::out);
@@ -14,7 +14,7 @@ gpio::gpioInit(int pin_number, string pin_direction, string pin_mode = "none")
     file.close();
 }
 
-gpio::gpioWritePin(int pin_number, int pin_status)
+ECC_gpio::GPIO::gpioWritePin(int pin_number, int pin_status)
 {
 	fstream file;
     file.open((string(path) + "/gpio" + string(pin_number) + "/value").c_str(), fstream::out);
@@ -22,12 +22,12 @@ gpio::gpioWritePin(int pin_number, int pin_status)
     file.close();
 }
 
-ECC_gpio::gpio::gpioReadPin(int pin_number)
+ECC_gpio::GPIO::gpioReadPin(int pin_number)
 {
 
 }
 
-ECC_gpio::gpio::~GPIO(int pin_number)
+ECC_gpio::GPIO::~GPIO(int pin_number)
 {
 
 }
