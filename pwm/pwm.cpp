0fca92466d2d80341fc276425a03b073226c7973
Tushar Gupta <64915891+TusharGuptaECC@users.noreply.github.com>
2023-03-20 03:48:12 +0530

    Update pwm.cpp

--- pwm/pwm.cpp
+++ pwm/pwm.cpp
@@ -8,28 +8,28 @@ PWM::PWM(std::string pin_number)
 void PWM::initPwm(std::string pwm_number, int period, int duty_cycle, std::string polarity)
 {
 	std::fstream file;
-    file.open((std::string(PWM_PATH) + std::string(pwm_number) + "/period").c_str(), std::fstream::out);
-    file << period;
-    file.close();
-    file.open((std::string(PWM_PATH) + std::string(pwm_number) + "/duty_cycle").c_str(), std::fstream::out);
-    file << duty_cycle;
-    file.close();
+	file.open((std::string(PWM_PATH) + std::string(pwm_number) + "/period").c_str(), std::fstream::out);
+    	file << period;
+    	file.close();
+    	file.open((std::string(PWM_PATH) + std::string(pwm_number) + "/duty_cycle").c_str(), std::fstream::out);
+    	file << duty_cycle;
+    	file.close();
 }
 
 void PWM::startPwm(std::string pwm_number)
 {
 	std::fstream file;
-    file.open((std::string(PWM_PATH) + std::string(pwm_number) + "/enable").c_str(), std::fstream::out);
-    file << 1;
-    file.close();
+    	file.open((std::string(PWM_PATH) + std::string(pwm_number) + "/enable").c_str(), std::fstream::out);
+    	file << 1;
+    	file.close();
 }
 
 void PWM::stopPwm(std::string pwm_number)
 {
 	std::fstream file;
-    file.open((std::string(PWM_PATH) + std::string(pwm_number) + "/enable").c_str(), std::fstream::out);
-    file << 0;
-    file.close();
+    	file.open((std::string(PWM_PATH) + std::string(pwm_number) + "/enable").c_str(), std::fstream::out);
+    	file << 0;
+    	file.close();
 }
 
 PWM::~PWM()
@@ -37,8 +37,8 @@ PWM::~PWM()
 	// uncomment if want to reset every pin to default value.
 	// changes can be made according to user requirement.
 	// std::fstream file;
-    // file.open((std::string(PWM_PATH) + std::string(pwm_number) + "/enable").c_str(), std::fstream::out);
-    // file << 0;
-    // file.close();
+    	// file.open((std::string(PWM_PATH) + std::string(pwm_number) + "/enable").c_str(), std::fstream::out);
+    	// file << 0;
+    	// file.close();
 	// system(("config-pin " + std::string(pin_number) + " default").c_str()); // reset pin to default 
 }
